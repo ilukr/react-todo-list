9c75642d432c910599d95b8df1ab20468cc73f96
kibalion23 <kibalion02@gmail.com>
2020-03-24 08:22:21 +0200

    adds ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css'
+import uuid from 'uuid'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
