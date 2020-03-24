13e2dbaf2c5dccfb18f353af0f2d7d5b43d5e513
kibalion23 <kibalion02@gmail.com>
2020-03-24 09:21:50 +0200

    delete list item

--- src/App.js
+++ src/App.js
@@ -43,6 +43,13 @@ class App extends Component {
     })
   }
 
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -54,7 +61,11 @@ class App extends Component {
               handleChange={ this.handleChange }
               handleSubmit={ this.handleSubmit }
             />
-            <TodoList items={ this.state.items } clearList={ this.clearList } />
+            <TodoList
+              items={ this.state.items }
+              clearList={ this.clearList }
+              handleDelete={ this.handleDelete }
+            />
           </div>
         </div>
       </div>
