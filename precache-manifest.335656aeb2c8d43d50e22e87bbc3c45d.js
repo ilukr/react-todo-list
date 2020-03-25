114b586f3af5a86282fcd716aa3bbdb69644d964
kibalion23 <kibalion02@gmail.com>
2020-03-25 08:32:43 +0200

    Updates

---
+++ precache-manifest.335656aeb2c8d43d50e22e87bbc3c45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17d5dcfccff57d9502b548388ffc3b4",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "95a86d0f3248ec65f9d3",
+    "url": "/react-todo-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e4e70de76139634d255f",
+    "url": "/react-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95a86d0f3248ec65f9d3",
+    "url": "/react-todo-list/static/js/2.d305d9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.d305d9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e70de76139634d255f",
+    "url": "/react-todo-list/static/js/main.3acde023.chunk.js"
+  },
+  {
+    "revision": "e7c9e688e31c89048346",
+    "url": "/react-todo-list/static/js/runtime-main.341947be.js"
+  }
+]);
